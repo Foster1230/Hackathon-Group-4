fde6586fc48fe5a588c678837c6576622ed2bb0d
Leo Craig <leozcraig@Leos-MacBook-Pro.local>
2020-07-21 09:29:13 -0600

    adding first winning conditions (come back to later)

--- logic.js
+++ logic.js
@@ -10,25 +10,36 @@ var suits = [
 var ID = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 var cardDeck = []
 
+
 for (var i = 0; i < ID.length; i++) {
-    for(var x = 0; x < suits.length; x++) {
+    for (var x = 0; x < suits.length; x++) {
         var value = parseInt(ID[i]);
-        if (ID [i] ===  "J" ||ID [i] === "Q" ||ID [i] === "K") {
+        if (ID[i] === "J" || ID[i] === "Q" || ID[i] === "K") {
             value = 10
-        } else if(ID[i] === "A") {
+        } else if (ID[i] === "A") {
             value = 11
         }
-        var card = {Suits: suits[x], ID: ID[i], Value: value}
+        var card = { Suits: suits[x], ID: ID[i], Value: value }
         cardDeck.push(card)
     }
 }
 
+
 console.log(cardDeck)
 console.log(card)
 
-var sevenC= {Suit:"Club", Value:"7",}
-var test= [sevenC];
+var sevenC = { Suit: "Club", Value: "7", }
+var test = [sevenC];
 
+//setting up winning game conditions
+function checkgame() {
+    if ((computerValue < playerValue < 21) || (computerValue > 21) || (playerValue === 21)) {
+        wins++
+        alert("the round goes to " + playerName + "!");
+    } else if ((playerValue < computerValue < 21) || (playerValue > 21) || (computerValue === 21)) {
+        lose++
+        alert("The dealer won this round!");
+    }
+}
 
-
-console.log(sevenC)
+    console.log(sevenC);
