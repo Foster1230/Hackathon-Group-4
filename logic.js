b2068d1764aeef1c1e0360d2839598c4d0840d45
Foster1230 <joshgreen2002@yahoo.com>
2020-07-20 21:33:18 -0400

    created a card deck - can be changed if necessary

--- logic.js
+++ logic.js
@@ -1,1 +1,34 @@
 alert("This is a test")
+
+
+var suits = [
+    "Clubs",
+    "Hearts",
+    "Spades",
+    "Diamonds"
+]
+var ID = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+var cardDeck = []
+
+for (var i = 0; i < ID.length; i++) {
+    for(var x = 0; x < suits.length; x++) {
+        var value = parseInt(ID[i]);
+        if (ID [i] ===  "J" ||ID [i] === "Q" ||ID [i] === "K") {
+            value = 10
+        } else if(ID[i] === "A") {
+            value = 11
+        }
+        var card = {Suits: suits[x], ID: ID[i], Value: value}
+        cardDeck.push(card)
+    }
+}
+
+console.log(cardDeck)
+console.log(card)
+
+var sevenC= {Suit:"Club", Value:"7",}
+var test= [sevenC];
+
+
+
+console.log(sevenC)
